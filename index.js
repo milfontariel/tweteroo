52e01d36825c41ecd104d3a2d12bef6b1e4d5c7a
ariel <ariel1242009@gmail.com>
2022-01-21 04:00:34 -0300

    back implementado

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+server.use(cors());
 
+let user = {};
+const tweets = [];
 
+server.post('/sign-up', (req, res) => {
+    user = req.body;
+    res.send("OK");
+});
+
+server.post('/tweets', (req, res) => {
+    const tweet = {
+        avatar: user.avatar,
+        username: req.body.username,
+        tweet: req.body.tweet
+    };
+    tweets.push(tweet);
+    console.log(user);
+    res.send("OK");
+})
+
+server.get('/tweets', (req, res) => {
+    if(tweets.length <= 10){
+        res.send(tweets)
+    } else {
+        const tempTweets = [];
+        for(let i = tweets.length - 1; i > tweets.length - 10; i--){
+            tempTweets.push(tweets[i]);
+        }
+        res.send(tempTweets);
+    }
+})
 
 server.listen(5000);
